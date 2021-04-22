d5b4528289186ae487aab777ffd91300d61dc2e7
henSetiawan <hendystyawan74@gmail.com>
2021-04-22 19:18:13 +0700

    create navbar component

---
+++ web_frontend_pmi/src/components/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="#">PMI Bondowoso</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Beranda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Stok Darah</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Daftar Relawan</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Tentang</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Artikel</a>
+        </li>
+        <li class="nav-item">
+          <a class="btn btn-sm btn-outline-secondary mt-2" href="#">Masuk</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "navbar",
+};
+</script>
+
+<style scoped>
+.navbar-nav {
+  padding: 10px;
+  display: flex;
+  align-content: center;
+}
+li a,
+.navbar-brand {
+  font-family: "Mulish", sans-serif;
+  font-weight: 400;
+  color: #1f1534 im !important;
+}
+</style>
--- web_frontend_pmi/src/main.js
+++ web_frontend_pmi/src/main.js
@@ -4,14 +4,13 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
-import Vue from 'vue'
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 
 Vue.config.productionTip = false;
 
