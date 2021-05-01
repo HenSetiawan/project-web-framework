ca62d0fee87d47c5625df96dfb8464c3f169c33f
henSetiawan <hendystyawan74@gmail.com>
2021-05-01 20:02:10 +0700

    create sidebar dashboard

--- web_frontend_pmi/src/router/index.js
+++ web_frontend_pmi/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Sidebar from "../Dashboard/Sidebar";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/sidebar",
+    name: "Sidebar",
+    component: Sidebar,
   },
 ];
 
