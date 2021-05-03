ebe213742ed3bcfdb417914eed938ce34ab2077c
fajar098123 <fajar098123@gmail.com>
2021-05-03 14:42:35 +0700

    make migration blogs

---
+++ Web_Api_PMI/database/migrations/2021_05_03_071524_create_blogs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul_blog');
+            $table->string('thumbnail');
+            $table->text('content');
+            $table->unsignedBigInteger('id_penulis');
+            $table->timestamps();
+
+            $table->foreign('id_penulis')->references('id')->on('admin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
