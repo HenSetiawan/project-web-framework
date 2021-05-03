ead086f9c98582102801ce8fcb3fe7fa115f2da0
fajar098123 <fajar098123@gmail.com>
2021-05-03 14:04:55 +0700

    make migration relawan and change migration admin

--- Web_Api_PMI/database/migrations/2021_05_03_065805_create_relawan_table.php
+++ Web_Api_PMI/database/migrations/2021_05_03_065805_create_relawan_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAdminTable extends Migration
+class CreateRelawanTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,13 @@ class CreateAdminTable extends Migration
      */
     public function up()
     {
-        Schema::create('admin', function (Blueprint $table) {
+        Schema::create('relawan', function (Blueprint $table) {
             $table->id();
             $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('gol_darah');
+            $table->text('alamat');
             $table->string('no_hp');
             $table->string('password');
             $table->rememberToken();
@@ -32,6 +34,6 @@ class CreateAdminTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admin');
+        Schema::dropIfExists('relawan');
     }
 }
