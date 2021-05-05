4b4c3b3eecce8b7ae8cfc2a7e96c6331863627c7
henSetiawan <hendystyawan74@gmail.com>
2021-05-05 11:50:42 +0700

    refactor user auth login

--- Web_Api_PMI/app/Http/Controllers/Users/AuthUsersController.php
+++ Web_Api_PMI/app/Http/Controllers/Users/AuthUsersController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\QueryException;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthUsersController extends Controller
@@ -18,21 +19,28 @@ class AuthUsersController extends Controller
             'email' => 'required|email',
             'password' => 'required',
         ]);
-        $user = User::where('email', $request->email)->first();
 
-        if(!$user || !Hash::check($request->password, $user->password)){
-            return response()->json([
-                'message' => 'Password Invalid'
-            ], Response::HTTP_UNAUTHORIZED);
-        }
-
-        $token = $user->createToken('token-name')->plainTextToken;
-
-        return response()->json([
-            'message' => 'Password Valid',
-            'user' => $user,
-            'token' => $token
-        ], Response::HTTP_OK);
+        try {
+            $user=User::where('email',$request->email)->first();
+            if($user!=null && Hash::check($request->password,$user->password)){
+                 $token=$user->createToken($user->email);
+                 return response()->json([
+                 "message"=>'succcess',
+                 "data"=>$user,
+                 "token"=>$token->plainTextToken
+                ])->setStatusCode(200);
+            }else{
+             return response()->json([
+                 "message"=>'email or password is wrong'
+                ])->setStatusCode(400);
+            }
+         } catch (QueryException $err) {
+             return response()->json([
+                 "message"=>"auth user failed",
+                 "data"=>$user,
+                 "error"=>$err->errorInfo
+             ])->setStatusCode(400);
+         }
     }
 
     public function register(Request $request)
