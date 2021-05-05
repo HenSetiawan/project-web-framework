aae8fdb92b83aaf0b33fcf7e904bf9a816ef50db
dwirifki10 <dwirifkinovianto10@gmail.com>
2021-05-05 13:00:50 +0700

    remove unused library

--- Web_Api_PMI/app/Http/Controllers/Users/AuthUsersController.php
+++ Web_Api_PMI/app/Http/Controllers/Users/AuthUsersController.php
@@ -6,9 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Database\QueryException;
-use Symfony\Component\HttpFoundation\Response;
 
 class AuthUsersController extends Controller
 {
@@ -21,31 +19,35 @@ class AuthUsersController extends Controller
         ]);
 
         try {
-            $user=User::where('email',$request->email)->first();
-            if($user!=null && Hash::check($request->password,$user->password)){
-                 $token=$user->createToken($user->email);
+
+            $user = User::where('email', $request->email)->first();
+
+            if($user != null && Hash::check($request->password, $user->password)){
+                 $token = $user->createToken($user->email);
                  return response()->json([
-                 "message"=>'succcess',
-                 "data"=>$user,
-                 "token"=>$token->plainTextToken
+                 "message" => 'succcess',
+                 "data" => $user,
+                 "token" => $token->plainTextToken
                 ])->setStatusCode(200);
+
             }else{
              return response()->json([
                  "message"=>'email or password is wrong'
                 ])->setStatusCode(400);
             }
+
          } catch (QueryException $err) {
              return response()->json([
-                 "message"=>"auth user failed",
-                 "data"=>$user,
-                 "error"=>$err->errorInfo
+                 "message" => "auth user failed",
+                 "data" => $user,
+                 "error" => $err->errorInfo
              ])->setStatusCode(400);
          }
     }
 
     public function register(Request $request)
     {
-        $user=$request->validate([
+        $user = $request->validate([
             'username' => ['required', 'max:50'],
             'email' => ['required', 'email', 'max:50'],
             'no_hp' => ['required', 'max:15'],
@@ -53,23 +55,25 @@ class AuthUsersController extends Controller
             'confirm_password' => ['required', 'same:password']
         ]);
 
-        $user['password']=Hash::make($user['password']);
+        $user['password'] = Hash::make($user['password']);
 
         try {
+
             User::create($user);
             return response()->json([
-                "message"=>"register user success",
-                "data"=>[
-                    "username"=>$user['username'],
-                    "email"=>$user['email'],
+                "message" => "register user success",
+                "data" => [
+                    "username" => $user['username'],
+                    "email" => $user['email'],
                 ],
-                "error"=>null,
+                "error" => null,
             ])->setStatusCode(200);
+
         } catch (QueryException $err) {
             return response()->json([
-                "message"=>"register user failed",
-                "data"=>$user,
-                "error"=>$err->errorInfo
+                "message" => "register user failed",
+                "data" => $user,
+                "error" => $err->errorInfo
             ])->setStatusCode(400);
         }
     }
