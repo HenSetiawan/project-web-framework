505c992acbeb4bbaf4bfe5f66b9e95178622813f
dwirifki10 <dwirifkinovianto10@gmail.com>
2021-06-07 20:34:15 +0700

    update model agenda and bloods

--- Web_Api_PMI/app/Models/Agenda.php
+++ Web_Api_PMI/app/Models/Agenda.php
@@ -4,12 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 
 class Agenda extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     protected $table = 'agenda';
 
