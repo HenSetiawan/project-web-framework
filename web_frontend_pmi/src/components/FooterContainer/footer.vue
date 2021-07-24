492335c00524c7e69e2c171c9bea6289d460eb33
henSetiawan <hendystyawan74@gmail.com>
2021-07-24 17:00:35 +0700

    add footer etc

--- web_frontend_pmi/src/components/FooterContainer/footer.vue
+++ web_frontend_pmi/src/components/FooterContainer/footer.vue
@@ -2,36 +2,31 @@
   <footer>
     <div class="container">
       <div class="row">
-        <div class="col-md-4 mt-4">
-          <h5><span class="circle-trafalgar text-center">T</span> Trafalgar</h5>
+        <div class="col-md-4 mt-5">
+          <h5>PMI Bondowoso</h5>
           <p class="mt-3">
-            Trafalgar provides progressive, and affordable healthcare,
-            accessible on mobile and online for everyone
+            Profesional, berintegritas dan bergerak bersama masyarakat
           </p>
-          <p>©Trafalgar PTY LTD 2020. All rights reserved</p>
+          <p>©PMI Bondowoso 2021. All rights reserved</p>
         </div>
         <div class="col-md-8 col-lg-8">
           <div class="row">
             <div class="col-md-4 mt-5 col-lg-4">
-              <h5>Company</h5>
+              <h5>Layanan</h5>
               <ul class="mt-4">
-                <li><a href="#">About</a></li>
-                <li><a href="#">Testimonials</a></li>
-                <li><a href="#">Find a Doctor</a></li>
+                <li><a href="#">Pantau Stok Darah</a></li>
+                <li><a href="#">Temukan Pendonor</a></li>
                 <li><a href="#">Apps</a></li>
               </ul>
             </div>
             <div class="col-md-4 mt-5 col-lg-4">
-              <h5>Region</h5>
+              <h5>Wilayah</h5>
               <ul class="mt-4">
-                <li><a href="#">Indonesia</a></li>
-                <li><a href="#">Singapore</a></li>
-                <li><a href="#">Hongkong</a></li>
-                <li><a href="#">Canada</a></li>
+                <li><a href="#">Bondowoso</a></li>
               </ul>
             </div>
             <div class="col-md-4 mt-5 col-lg-4">
-              <h5>Help</h5>
+              <h5>Bantuan</h5>
               <ul class="mt-4">
                 <li><a href="#">Help Center</a></li>
                 <li><a href="#">Contact Suport</a></li>
@@ -47,7 +42,7 @@
 </template>
 
 <style scoped>
-p{
+p {
   color: white !important;
 }
 </style>
